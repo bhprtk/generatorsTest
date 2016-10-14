2bcaa73629c5b67618b9bfa39c1aa75adedd840a
pratik <lazy.monk.13@gmail.com>
2016-10-14 15:30:15 -0700

    fulfull imlementation of co

--- generators.js
+++ generators.js
@@ -1,15 +1,28 @@
 const fetch = require('node-fetch');
-const co = require('co');
+// const co = require('co');
 
 // fetch('http://jsonplaceholder.typicode.com/posts/1')
 // 	.then(response => response.json())
 // 	.then(post => post.title)
 // 	.then(title => console.log ('title:', title))
 
-co(function *() {
+run(function *() {
 	const uri = 'http://jsonplaceholder.typicode.com/posts/1';
 	const response = yield fetch(uri);
+	console.log ('response:', response)
 	const post = yield response.json();
+	console.log ('post:', post)
 	const title = post.title;
 	console.log ('title:', title);
-})
+});
+
+function run(generator) {
+	const iterator = generator();
+	const iteration = iterator.next();
+	const promise = iteration.value;
+	promise.then(x => {
+		const anotherIterator = iterator.next(x);
+		const anotherPromise = anotherIterator.value;
+		anotherPromise.then(y => iterator.next(y))
+	});
+}
