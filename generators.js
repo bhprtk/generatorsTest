52b80df278d2f186f25a3800288aa8e0953ac141
pratik <lazy.monk.13@gmail.com>
2016-10-15 07:53:47 -0700

    everything working before mpg rampage 😂

--- generators.js
+++ generators.js
@@ -1,10 +1,4 @@
 const fetch = require('node-fetch');
-// const co = require('co');
-
-// fetch('http://jsonplaceholder.typicode.com/posts/1')
-// 	.then(response => response.json())
-// 	.then(post => post.title)
-// 	.then(title => console.log ('title:', title))
 
 run(function *() {
 	const uri = 'http://jsonplaceholder.typicode.com/posts/1';
@@ -23,6 +17,6 @@ function run(generator) {
 	promise.then(x => {
 		const anotherIterator = iterator.next(x);
 		const anotherPromise = anotherIterator.value;
-		anotherPromise.then(y => iterator.next(y))
+		anotherPromise.then(post => iterator.next(post))
 	});
 }
